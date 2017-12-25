ca354fcd2c4feb9a95f3258f85973c2e997b10b6
Shalom Naim <snaim@redhat.com>
2017-12-25 08:00:58 -0500

    adding lease support

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -x
 
+python /render_env.py
+
 echo "Decrypting credentials"
 pushd integration_tests
 python scripts/encrypt_conf.py -d --file credentials
+python lease_appliance.py
 
 echo "Rendering env.yaml"
 popd
